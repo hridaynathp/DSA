3ff84b598fb4b09ec182721a783d89c3e13a373d
hridaynath <hridaynathp@gmail.com>
2020-11-18 17:32:10 +0530

    Few updates with delete an element functionality

--- arrayInsertAtPosition.cpp
+++ arrayInsertAtPosition.cpp
@@ -15,8 +15,8 @@ void display(int* arr, int size){
 }
 
 // Logic to insert an element elmnt at position pos
-int* insertAnElement(int size, int arr[], int elmnt, int pos) {
-    
+void insertAnElement(int arr[], int size, int elmnt, int pos) {
+
     size++;
     int i=0;
     for(i=size; i>=pos; i--){
@@ -26,23 +26,32 @@ int* insertAnElement(int size, int arr[], int elmnt, int pos) {
     display(arr,size); //Your array is : 1	42	2	3	4	5
 }
 
+void deleteAnElement(int arr[], int size, int pos){
+    for(int i=pos-1; i<size; i++){
+        arr[i]=arr[i+1];
+    }
+    display(arr, size-1);
+}
+
 
 int main() {
 	// your code goes here.
-  
+
 	int size = 5;
-	int arr[5];
-	
-	// Taking input for array from user 
+	int arr[10]={0};
+
+	// Taking input for array from user
 	for(int i=0; i<size; i++){
 	    cin>>arr[i];
 	}
-	
+
+	//Display original array
 	display(arr,5); //Your array is : 1	2	3	4	5
-  
-	
+
 	// Adding element 42 to array
-	insertAnElement(size,arr,42,2);
-	
+	insertAnElement(arr,size,42,2);
+
+	deleteAnElement(arr, size+1, 2);
+
 	return 0;
 }
